5f90d0b8f0a97e0764d15b91e79272815e7911c0
Koy Saeteurn <kosatun@aol.com>
2018-08-06 02:37:52 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,8 +7,8 @@ class Scraper
   def self.scrape_index_page(index_url)
     doc = Nokogiri:HTML(index_url)
     students = docs.css(.student-card)
-    name = students.css(.student-name)
-    
+    name = students.css(.student-name).map {|name| name.text}.compact
+    binding.pry
   end
 
   def self.scrape_profile_page(profile_url)
