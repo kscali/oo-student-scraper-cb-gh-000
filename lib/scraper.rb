d9f0ead37d7c3dfebaf3e1c7f5e34294b3d0362e
Koy Saeteurn <kosatun@aol.com>
2018-08-06 03:05:59 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,33 +1,41 @@
 require 'open-uri'
-require 'pry'
-require 'nokogiri'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
-    doc = Nokogiri:HTML(index_url)
+    index_page = Nokogiri::HTML(open(index_url))
     students = []
-    
-    docs.css('div.roster-cards-container').each do |card|
-      card.css('.student-name a').each do |student|
+    index_page.css("div.roster-cards-container").each do |card|
+      card.css(".student-card a").each do |student|
         student_profile_link = "#{student.attr('href')}"
         student_location = student.css('.student-location').text
         student_name = student.css('.student-name').text
-        
         students << {name: student_name, location: student_location, profile_url: student_profile_link}
-      end 
-    end 
+      end
+    end
     students
   end
 
-  def self.scrape_profile_page(profile_url)
-    doc = Nokogiri::HTML(profile_url)
-    social_links = []
-    
-    doc.css(div.social-icon-container a).each do |social|
-      twitter_link = "#{social.attr('href')}"
-    
+  def self.scrape_profile_page(profile_slug)
+    student = {}
+    profile_page = Nokogiri::HTML(open(profile_slug))
+    links = profile_page.css(".social-icon-container").children.css("a").map { |el| el.attribute('href').value}
+    links.each do |link|
+      if link.include?("linkedin")
+        student[:linkedin] = link
+      elsif link.include?("github")
+        student[:github] = link
+      elsif link.include?("twitter")
+        student[:twitter] = link
+      else
+        student[:blog] = link
+      end
+    end
+   
+    student[:profile_quote] = profile_page.css(".profile-quote").text if profile_page.css(".profile-quote")
+    student[:bio] = profile_page.css("div.bio-content.content-holder div.description-holder p").text if profile_page.css("div.bio-content.content-holder div.description-holder p")
+
+    student
   end
 
 end
-
